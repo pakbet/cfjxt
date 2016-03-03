2e80648d91944ca8bf34f8d8e94040770a81e3ce
Pakbet <lulu@bauson.com>
2016-03-03 08:27:20 +0800

    test3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', val1: 'Jay', val2: 'Ar', val3: 'Bauson' });
 });
 
 module.exports = router;
